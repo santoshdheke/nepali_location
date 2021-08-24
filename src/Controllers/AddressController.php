ddc4a5948ad2d0d70f9e0ecbd6a58ad7f2a33512
santosh dheke <santoshdheke46@gmail.com>
2021-08-24 11:56:54 +0545

    update

--- src/Controllers/AddressController.php
+++ src/Controllers/AddressController.php
@@ -1,30 +1,33 @@
 <?php
 
-    namespace Ssgroup\Address\Controllers;
+namespace Ssgroup\Address\Controllers;
 
 use Ssgroup\Address\Model\Municipality;
 use Ssgroup\Address\Model\Province;
 use Ssgroup\Address\Model\State;
-use App\Http\Controllers\Controller;
+use Ssgroup\ApiManager\Controllers\ApiBaseController;
 
-class AddressController extends Controller
+class AddressController extends ApiBaseController
 {
 
     public function province()
     {
         $provinces = Province::get();
+        return $this->sendDataResponse($provinces);
         return view('address::province',compact('provinces'));
     }
 
     public function state($id)
     {
         $states = State::where('province_id',$id)->get();
+        return $this->sendDataResponse($states);
         return view('address::district',compact('states'));
     }
 
     public function municipality($id)
     {
         $municipalities = Municipality::where('state_id',$id)->get();
+        return $this->sendDataResponse($municipalities);
         return view('address::municipality',compact('municipalities'));
     }
 
--- src/view/index.blade.php
+++ src/view/index.blade.php
@@ -1,4 +1,3 @@
-
 <div class="form-group" id="province_div">
     <label for="">Province</label>
     <select name="province" class="form-control">
@@ -31,24 +30,83 @@
 
 @push('js')
     <script>
+        function provienceHtml(datas) {
+
+            var html = `<label for="">Province</label>
+                <select name="province" class="form-control" id="province">
+                    <option value="">Choose Province</option>`;
+
+            datas.data.forEach(function (data, key) {
+                html = html + `<option value="${data.id}">${data.title}</option>`;
+            })
+
+            html = html + `</select>
+                @if(isset($errors) && $errors->has('province'))
+                <code>{{ $errors->first('province') }}</code>
+                @endif
+                `;
+
+            return html;
+        }
+
+        function districtHtml(datas) {
+
+            var html = `<label for="">District</label>
+                <select name="district" id="district" class="form-control">
+                    <option value="">Choose District</option>`;
+
+            datas.data.forEach(function (data, key) {
+                html = html + `<option value="${data.id}">${data.title}</option>`;
+            })
+
+            html = html + `</select>
+                @if(isset($errors) && $errors->has('district'))
+                <code>{{ $errors->first('district') }}</code>
+                @endif`;
+
+            return html;
+        }
+
+        function municipalityHtml(datas) {
+
+            var html = `<label for="">Municipality</label>
+                <select name="municipality" class="form-control" id="municipality">
+                    <option value="">Choose Municipality</option>`;
+
+            datas.data.forEach(function (data, key) {
+                html = html + `<option value="${data.id}">${data.title}</option>`;
+            })
+
+            html = html + `</select>
+                @if(isset($errors) && $errors->has('municipality'))
+                <code>{{ $errors->first('municipality') }}</code>
+                @endif
+                `;
+
+            return html;
+        }
+
         $(document).ready(function () {
-            $.get('{{ url('address-package/province') }}',function (view) {
+            $.get('{{ url('address-package/province') }}', function (data) {
+                var view = provienceHtml(data);
                 $('#province_div').html(view);
             });
 
-            $(document).on('change','#province',function () {
+            $(document).on('change', '#province', function () {
                 var id = $(this).val();
-                $.get('{{ url('address-package/state') }}'+'/'+id,function (view) {
+                $.get('{{ url('address-package/state') }}' + '/' + id, function (data) {
+                    var view = districtHtml(data);
                     $('#district_div').html(view);
                 });
             });
 
-            $(document).on('change','#district',function () {
+            $(document).on('change', '#district', function () {
                 var id = $(this).val();
-                $.get('{{ url('address-package/municipality') }}'+'/'+id,function (view) {
+                $.get('{{ url('address-package/municipality') }}' + '/' + id, function (data) {
+                    var view = municipalityHtml(data);
                     $('#municipality_div').html(view);
                 });
             });
         });
     </script>
-    @endpush
+@endpush
